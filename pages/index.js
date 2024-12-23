c9c8fc18f19bc3e57650d3b9be65db59ee350654
Daniel Peñafiel <luis.chavez.penafiel@uagraria.edu.ec>
2024-12-22 22:24:59 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,18 @@
 import Link from 'next/link';
 
 export default function Home() {
-    return ( <
-        div style = {
-            { textAlign: 'center', padding: '50px' } } >
-        <
-        h1 > Bienvenido a la aplicación < /h1> <
-        p > Por favor, selecciona una opción: < /p> <
-        div style = {
-            { marginTop: '20px' } } >
-        <
-        Link href = "/register" >
-        <
-        a style = {
-            { margin: '0 10px', textDecoration: 'none', color: 'blue' } } > Registro < /a> <
-        /Link> <
-        Link href = "/login" >
-        <
-        a style = {
-            { margin: '0 10px', textDecoration: 'none', color: 'blue' } } > Iniciar Sesión < /a> <
-        /Link> <
-        /div> <
-        /div>
-    );
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Bienvenido a la aplicación</h1>
+      <p>Por favor, selecciona una opción:</p>
+      <div style={{ marginTop: '20px' }}>
+        <Link href="/register">
+          <a style={{ margin: '0 10px', textDecoration: 'none', color: 'blue' }}>Registro</a>
+        </Link>
+        <Link href="/login">
+          <a style={{ margin: '0 10px', textDecoration: 'none', color: 'blue' }}>Iniciar Sesión</a>
+        </Link>
+      </div>
+    </div>
+  );
 }
